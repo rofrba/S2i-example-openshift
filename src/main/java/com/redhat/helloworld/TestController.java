a7273dc4df47f8e8729aac0025ac13e03788e59e
Rodrigo Alvarez <ralvarez@redhat.com>
2020-06-16 15:38:09 -0300

    First commit

---
+++ src/main/java/com/redhat/helloworld/TestController.java
@@ -0,0 +1,14 @@
+package com.redhat.helloworld;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping(path = "/test")
+    public ResponseEntity<String> sayHello()  {  
+         
+        return ResponseEntity.ok("Hello World!");
+    }
+}
